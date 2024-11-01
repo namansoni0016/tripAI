d82bd1a682d8938d0551985777353c625f864d1e
Naman Soni <namantsoni@gmail.com>
2024-11-01 12:51:33 +0530

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl font-semibold">Welcome!</h1>
-      <Button>Get Started...</Button>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-800 to-black">
+      <div className="space-y-1 text-center text-gray-200">
+        <h1 className="text-6xl font-semibold">
+          Welcome to, <span className="text-6xl font-semibold text-red-600 drop-shadow-md">TripAI</span>
+        </h1>
+        <p className="text-xl font-medium">Your journey, magically planned!</p>
+        <Button asChild variant="link">
+          <Link href="/register" className="text-xl text-blue-400 hover:text-blue-700">Get started...</Link>
+        </Button>
+      </div>
+    </main>
   );
 }
