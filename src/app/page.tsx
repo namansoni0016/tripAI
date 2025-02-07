85b5c68d7a274cd7195654f725120db722d08332
Naman Soni <namantsoni@gmail.com>
2025-02-07 13:06:42 +0530

    Modified HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { RegisterButton } from "@/components/auth/register-button";
-import { useSession, getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import AuthenticatedHome from "@/components/auth/AuthenticatedHome";
 
 const font = Poppins({
@@ -13,26 +13,27 @@ const font = Poppins({
 
 export default function Home() {
   const session = useSession();
-  const updatedSession = getSession();
   return (
-    <main className="flex h-full flex-col items-center justify-center">
+    <>
       {session.data ? (
         <AuthenticatedHome />
       ) : (
-        <div className="space-y-6 text-center">
+      <main className="flex h-full flex-col items-center justify-center">
+        <div className="space-y-4 text-center">
           <h1 className={cn("text-3xl md:text-6xl font-semibold text-white drop-shadow-md", font.className)}>
             Welcome to, <span className="text-3xl md:text-6xl font-semibold text-red-600">TripAI</span>
           </h1>
-          <p className="text-white md:text-lg">Your journey, magically planned!</p>
+          <p className="text-white md:text-xl font-semibold">Your journey, magically planned!</p>
           <div>
             <RegisterButton>
-              <Button variant="secondary" size="lg">
+              <Button variant="secondary" className="text-lg font-semibold p-5 rounded-full mt-2 transition-transform duration-300 ease-in-out hover:translate-y-1" >
                 Get Started
               </Button>
             </RegisterButton>
           </div>
         </div>
+      </main>
       )}
-    </main>
+    </>
   );
 }
--- src/components/auth/AuthenticatedHome.tsx
+++ src/components/auth/AuthenticatedHome.tsx
@@ -1,6 +1,27 @@
+"use client"
+import { Input } from "@/components/ui/input";
+import { IoIosSend } from "react-icons/io";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
 const AuthenticatedHome = () => {
+    const [query, setQuery] = useState("");
     return (
-        <div>AuthenticatedHome</div>
+        <div className="flex flex-col items-center justify-center mt-8">
+            <h2 className="text-4xl font-bold text-white">Generate your perfect trip!</h2>
+            <div className="flex flex-row mt-4 w-[800px] bg-white border-none rounded-full px-4 py-2 items-center">
+                {/* <Input placeholder="Type here..." value={query} 
+                    onChange={(e) => setQuery(e.target.value)} 
+                    className="flex-1 p-6 rounded-full border-none focus-visible:ring-0 text-lg" 
+                    aria-multiline="true"/> */}
+                    <textarea placeholder="Message TripAI" value={query} onChange={(e) => setQuery(e.target.value)}
+                        className="flex-1 py-3 px-3 rounded-full border-none focus-visible:ring-0 text-lg bg-white max-h-[200px] min-h-[50px] outline-none overflow-y-auto resize-none"
+                        rows={1} />
+                <Button variant={"outline"} className="flex items-center justify-center rounded-full border-none shadow-none p-3">
+                    <IoIosSend className="w-10 h-10 text-gray-600 cursor-pointer hover:text-gray-500 transition-colors duration-300 transform scale-150"/>
+                </Button>
+            </div>
+        </div>
     );
 }
 
